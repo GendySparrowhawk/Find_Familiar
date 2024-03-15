bf7bb06cb279de06758acea075144a3f2590fe93
jacob hoefer <jacob.hoefer@gmail.com>
2024-03-15 13:00:25 -0400

    profie tweaks

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -47,32 +47,126 @@ function Auth({ isLogin }) {
     variables: formData,
   });
 
-  const handleSubmit = async (e)=> {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     try {
-        const resolverName = isLogin ? 'login' : 'register'
-        const { data: userData } = await authenticateUser()
-        setFormData({ ...initalFormData})
+      const resolverName = isLogin ? "login" : "register";
+      const { data: userData } = await authenticateUser();
+      setFormData({ ...initalFormData });
 
-        setState(oldState => ({
-            ...oldState,
-            user: userData[resolverName]
-        }))
-        setErrorMessage('')
-        navigate("/")
-    } catch(err) {
-        setErrorMessage(err.message)
+      setState((oldState) => ({
+        ...oldState,
+        user: userData[resolverName],
+      }));
+      setErrorMessage("");
+      navigate("/");
+    } catch (err) {
+      setErrorMessage(err.message);
     }
-  }
+  };
 
   return (
     <>
-    <section className='auth'> 
+      <section className="auth">
         <form onSubmit={handleSubmit}>
-            <h2></h2>
+          <h2>{isLogin ? "Log In" : "Register"}</h2>
+          {errorMessage ? <p>{errorMessage}</p> : ""}
+
+          {isLogin ? (
+            <>
+              <div className="mb-3">
+                <label
+                  htmlFor="formBasicIdentifier"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Username or Email:
+                </label>
+                <input
+                  id="formBasicIdentifier"
+                  type="text"
+                  name="identifier"
+                  value={formData.identifier}
+                  onChange={handleInputChange}
+                  placeholder="Enter username or email"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="mb-3">
+                <label
+                  htmlFor="formBasicUsername"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Username:
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  placeholder="Enter username"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label
+                  htmlFor="formBasicEmail"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email:
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Enter Email"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="formBasicPassword"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password:
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="Passowrd must include one: !@#$%^&*()"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </>
+          )}
+          <div className="nav-control mb-3 d-flex">
+            {isLogin ? (
+              <>
+                <span className="me-1">Don't have an Account?</span>
+                <NavLink to="/register">Register</NavLink>
+              </>
+            ) : (
+              <>
+                <span className="me-1">Already have an Account?</span>
+                <NavLink to="/login">Log In</NavLink>
+              </>
+            )}
+          </div>
+
+          <Button className="my-btn" type="submit">
+            Submit
+          </Button>
         </form>
-    </section>
+      </section>
     </>
-  )
+  );
 }
+
+export default Auth;
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { useStore } from "../store";
+import { useNavigate } from "react-router-dom";
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+import ProfileImageUpload from "../components/ProfileImageUpload";
+
+function Profile() {
+  const { user, setState } = useStore();
+  const [userId, setUserId] = useState("");
+  const [showProfileImageUpload, setShowProfileImageUpload] = useState(false);
+
+  const navigate = useNavigate();
+
+  // PROFILE PICUTRE UPLOAD
+  const toggleProfileImageUpload = () => {
+    setShowProfileImageUpload(!showProfileImageUpload);
+  };
+
+  const handleProfileImageUpload = async (selectedImage) => {
+    try {
+      await uploadProfilePicture({
+        variables: {
+          id: user.id,
+          profilePicture: selectedImage,
+        },
+      });
+
+      setShowProfileImageUpload(false);
+    } catch (err) {
+      console.error("Error uploading profile picture:", err);
+    }
+  };
+
+  //   NAVIGATE TO LOGIN
+  function toLogin() {
+    navigate("/login");
+  }
+
+  //   PROFILE PAGE
+  return (
+    <>
+      {!user ? (
+        toLogin()
+      ) : (
+        <section className="Profile">
+          <div className="mb-3">
+            {showProfileImageUpload && (
+              <ProfileImageUpload
+                onUpload={handleProfileImageUpload}
+                toggle={toggleProfileImageUpload}
+              />
+            )}
+          </div>
+          <div>
+            <h2>Wlecome {user.username}</h2>
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
+
+export default Profile;
